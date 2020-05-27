b7e1b88782fff533f29164fd5003f8db5560fc9d
Guber <orga.gbr@posteo.de>
2020-05-27 22:32:25 +0200

    Implement Endpoint to Fetch Product By Category

--- Backend/app.js
+++ Backend/app.js
@@ -10,4 +10,18 @@ app.get("/", (req, res) => {
   db.query("SELECT * FROM CATEGORY").then((e) => res.json(e.rows));
 });
 
+/*
+* SELECT product_name FROM product LEFT JOIN category ON product.product_category = category.category_id
+  WHERE category.category = 'Thinkers'
+* */
+app.get("/products/:tag", (req, res) => {
+  const { tag } = req.params;
+  db.query(
+    "SELECT * FROM product LEFT JOIN category ON (product.product_category = category.category_id) WHERE category.category iLike $1",
+    [tag]
+  )
+    .then((products) => res.json(products.rows))
+    .catch((error) => console.log(error));
+});
+
 app.listen(5000, () => console.log("Server is running on port: 5000"));
