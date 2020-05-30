0af5532e566e2cf183f32bc546a99ca3c4aa32af
Samuel Greenwald <gozwald@gmail.com>
2020-05-30 16:46:11 +0200

    get all posts route complete

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,23 @@ require("dotenv").config();
 const db = require("./database");
 const app = express();
 const cors = require("cors");
-console.log("hey");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/blog/getall", (req, res) => {
+  db.query(
+    "SELECT author_name, author_avatar, author_description, post_date, post_type, post_title, post_content FROM authors JOIN posts ON author_id=post_author_id"
+  )
+    .then((e) => res.json(e.rows))
+    .catch((error) => console.log(error));
+});
+
 app.get("/category", (req, res) => {
-  db.query("SELECT * FROM CATEGORY").then((e) => res.json(e.rows));
+  db.query("SELECT * FROM CATEGORY")
+    .then((e) => res.json(e.rows))
+    .catch((error) => console.log(error));
 });
 
 app.get("/products", (req, res) => {
@@ -17,18 +28,18 @@ app.get("/products", (req, res) => {
 
   query.num
     ? db
-      .query(
-        "SELECT product_id, product_description, product_name, category FROM product JOIN category ON product_category = category_id LIMIT $1",
-        [limit]
-      )
-      .then((data) => res.json(data.rows))
-      .catch(console.log)
+        .query(
+          "SELECT product_id, product_description, product_name, category FROM product JOIN category ON product_category = category_id LIMIT $1",
+          [limit]
+        )
+        .then((data) => res.json(data.rows))
+        .catch(console.log)
     : db
-      .query(
-        "SELECT product_id, product_description, product_name, category FROM product JOIN category ON product_category = category_id"
-      )
-      .then((data) => res.json(data.rows))
-      .catch(console.log);
+        .query(
+          "SELECT product_id, product_description, product_name, category FROM product JOIN category ON product_category = category_id"
+        )
+        .then((data) => res.json(data.rows))
+        .catch(console.log);
 });
 
 app.get("/products/:category", (req, res) => {
